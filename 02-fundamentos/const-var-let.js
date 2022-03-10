f8f8ba3061e079448bf5974c53dbdc331b822928
Cristian <cristian.viragor@gmail.com>
2022-03-10 08:24:23 -0300

    09-03-2022

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,14 @@
+const nombre = 'Wolverine';
+
+if ( true ) {
+    const nombre = 'Magneto';
+}
+
+
+
+
+
+console.log (nombre);
+
+//Const se utiliza si el nombre de una Variable no va cambiar
+//let se utiliza cuando la variable si va cambiar
