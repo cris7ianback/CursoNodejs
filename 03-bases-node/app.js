faac4982a5a6db95ad1878f6f63fd0ad55699b10
Cristian Virago <cristian.viragor@gmail.com>
2022-08-17 12:32:34 -0400

    17-08-2022

---
+++ 03-bases-node/app.js
@@ -0,0 +1,25 @@
+
+const { crearArchivoTabla } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
+
+console.clear();
+
+console.log (process.argv);
+console.log (argv);
+
+console.log('base: yargs', argv.base)
+
+
+
+
+// const base = 2;
+
+
+// crearArchivoTabla(base)
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//     .catch(err => console.log(err));
+
+
+
+
+
