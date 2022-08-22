9dc9439b638d1c45b7838a6ddbe68ca469e8704a
Cristian Virago <cristian.viragor@gmail.com>
2022-08-22 14:50:49 -0400

    Sección 6
    Sección 6 Finalizada.

--- 05-clima-app/helpers/inquirer.js
+++ 05-clima-app/helpers/inquirer.js
@@ -29,7 +29,7 @@ const inquirerMenu = async () => {
 
     console.clear();
     console.log('==========================='.green);
-    console.log('   Seleccione una opción'.white);
+    console.log('   Seleccione una opción'.yellow);
     console.log('===========================\n'.green);
 
     const { opcion } = await inquirer.prompt(menuOpts);
@@ -53,7 +53,8 @@ const pausa = async () => {
 
 const leerInput = async (message) => {
 
-    const question = [{
+    const question = [
+        {
         type: 'input',
         name: 'desc',
         message,
@@ -69,21 +70,18 @@ const leerInput = async (message) => {
     return desc;
 }
 
-const listadoTareasBorrar = async (tareas = []) => {
-
-    const choices = tareas.map((tarea, i) => {
-
+const listarLugares = async (lugares = []) => {
+    const choices = lugares.map((lugar, i) => {
         const idx = `${i + 1}.`.green;
 
         return {
-            value: tarea.id,
-            name: `${idx} ${tarea.desc}`
-
+            value: lugar.id,
+            name: `${ idx } ${ lugar.nombre }`
         }
     });
 
     choices.unshift({
-        value: '',
+        value: '0',
         name: '0.'.green + 'Cancelar'
 
     })
@@ -92,7 +90,7 @@ const listadoTareasBorrar = async (tareas = []) => {
         {
             type: 'list',
             name: 'id',
-            message: 'Borrar',
+            message: 'Seleccione lugar: ',
             choices
         }
     ]
@@ -150,7 +148,7 @@ export {
     confirmar,
     inquirerMenu,
     leerInput,
-    listadoTareasBorrar,
+    listarLugares,
     mostrarListadoCheckList,
     pausa
 };
--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,19 +1,59 @@
-import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js";
+
+import 'dotenv/config';
+import { inquirerMenu, leerInput, listarLugares, pausa } from "./helpers/inquirer.js";
+import { Busquedas } from './models/busquedas.js';
 
 
 const main = async () => {
-
+    const busquedas = new Busquedas();
     let opt;
 
     do {
         opt = await inquirerMenu();
-        console.log({ opt });
+
+        switch (opt) {
+            case 1:
+                //Mostrar mensaje
+                const termino = await leerInput('Ciudad: ');
+
+                //buscar el lugar 
+                const lugares = await busquedas.ciudad(termino);
+
+                //seleccionar el lugar
+                const id = await listarLugares(lugares);
+                if( id === '0') continue;
+
+                const lugarSel = lugares.find(l => l.id === id);
+
+                //guardar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
+
+                // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng)                
+
+                //mostrar resultados
+                console.clear();
+                console.log('\nInformació de la ciudad \n'.green);
+                console.log('Ciudad:', lugarSel.nombre.green);
+                console.log('Lat:', lugarSel.lat);
+                console.log('Lng:', lugarSel.lng);
+                console.log('Temperatura:', clima.temp );
+                console.log('Minima:'.blue, clima.min);
+                console.log('Maxima:'.red,clima.max);
+                console.log('Como está el clima:',clima.desc.green);
+                break;
+
+                case 2:
+                    busquedas.historialCapitalizado.forEach( (lugar, i) =>{
+                        const idx =`${i + 1}.`.green;
+                        console.log( `${ idx } ${ lugar } `);
+                    })
+                    break;
+        }
 
         if (opt !== 0) await pausa();
 
-    } while (opt !== 0) await pausa();
-
-
+    } while (opt !== 0)
 
 }
 
