591a3cfb5c52886f04c2d66329c1556e421ed8ef
Cristian Virago <cristian.viragor@gmail.com>
2022-08-23 11:27:51 -0400

    Sección 7
    Sección 7 Finalizada,  Se comienza Sección 8.
    Clase 99

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,28 +1,47 @@
 import express from 'express'
-const app = express();
-const port = 8080;
-
+import hbs from 'hbs';
 import path from 'path';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
 const __dirname = path.resolve();
+const port = process.env.PORT;
+
+//Handlebar
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 //Servir contenido estático
 app.use(express.static('public'));
 
-app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
-});
+// app.get('/', (req, res) => {
+//     res.render('home', {
+//         nombre: 'Cristian Virago',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
-});
+// app.get('/generic', (req, res) => {
+//     res.render('generic', {
+//         nombre: 'Cristian Virago',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectiva ruta');
-});
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Cristian Virago',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    res.sendFile( __dirname + '/public/index.html')
 });
+
 
 app.listen(port, () => {
     console.log(`Aplicación corriendo en http://localhost:${port}`)
